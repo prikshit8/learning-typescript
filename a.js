73049bdc115b6302054a5d2c8cc93d5b00064946
prikshit8 <prikshitchawla.v@gmail.com>
2024-05-04 15:38:00 +0530

    basic types

---
+++ a.js
@@ -0,0 +1,8 @@
+"use strict";
+// let x:number=1;
+// console.log(x);
+// number, string, any
+function greet(firstName, lastName) {
+    console.log('hello' + firstName);
+}
+greet('prikshit', 'chawla');
---
+++ a.ts
@@ -0,0 +1,10 @@
+// let x:number=1;
+// console.log(x);
+
+
+// number, string, any
+function greet(firstName: string, lastName: string) {
+    console.log('hello' + firstName);
+}
+
+greet('prikshit','chawla');
