37a83929bd497c47e4b0db3cc126d72e3d023d6b
prikshit8 <prikshitchawla.v@gmail.com>
2024-05-04 15:51:41 +0530

    type of callback

--- a.ts
+++ a.ts
@@ -2,9 +2,34 @@
 // console.log(x);
 
 
-// number, string, any
-function greet(firstName: string, lastName: string) {
-    console.log('hello' + firstName);
+// // number, string, any
+// function greet(firstName: string, lastName: string) {
+//     console.log('hello' + firstName);
+// }
+
+// greet('prikshit','chawla');
+
+// type inference => TS was able to automatically
+// identify the type of return of sum
+
+// function sum(a: number, b: number): number {
+//     return a + b;
+// }
+
+// const value = sum(10, 20);
+// console.log(value);
+
+
+// function isLegal(age: number): boolean {
+//     return age > 18;
+// }
+
+// let x: boolean = isLegal(20);
+
+function hof(fn: Function) {
+    setTimeout(fn, 1000);
 }
 
-greet('prikshit','chawla');
+hof(function () {
+    console.log('hi there');
+})
